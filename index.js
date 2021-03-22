c3acfb269bf66c899189bfcf1b165071d724ad8f
ohaiklochan <caforonda103@yahoo.com>
2021-03-22 17:16:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 1
+let num2 = 62
+let num3 = 9
+let num4 = 5
+
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3%num4;
+
+const max = 20;
